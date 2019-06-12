3d4ffea626cd7397c2c5c41ea656c6c9c0b48ab0
Nikolaos Moraitis <nmoraiti@redhat.com>
2019-06-12 12:37:45 +0200

    update main for the periodics

--- cmd/pj-rehearse/main.go
+++ cmd/pj-rehearse/main.go
@@ -289,16 +289,16 @@ func rehearseMain() int {
 	if len(presubmits) == 0 && len(changedPeriodics) == 0 {
 		logger.Info("no jobs to rehearse have been found")
 		return 0
-	} else if len(rehearsals) > o.rehearsalLimit {
+	} else if len(presubmits) > o.rehearsalLimit {
 		jobCountFields := logrus.Fields{
 			"rehearsal-threshold": o.rehearsalLimit,
-			"rehearsal-jobs":      len(rehearsals),
+			"rehearsal-jobs":      len(presubmits) + len(changedPeriodics),
 		}
 		logger.WithFields(jobCountFields).Info("Would rehearse too many jobs, will not proceed")
 		return 0
 	}
 
-	executor := rehearse.NewExecutor(rehearsals, prNumber, o.releaseRepoPath, jobSpec.Refs, o.dryRun, loggers, pjclient)
+	executor := rehearse.NewExecutor(presubmits, periodics, prNumber, o.releaseRepoPath, jobSpec.Refs, o.dryRun, loggers, pjclient)
 	success, err := executor.ExecuteJobs()
 	metrics.Execution = executor.Metrics
 	if err != nil {
